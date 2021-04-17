761a98a2b4ff3b5bb1fa87e4b5e46e1dd8d5d74d
jishnu <jishnuanchery@gmail.com>
2021-04-17 16:49:49 +0530

    Made changes to to the list item in the stock page.

--- lib/pages/instockpage.dart
+++ lib/pages/instockpage.dart
@@ -3,30 +3,41 @@ import 'package:flutter/material.dart';
 import 'package:quickumber/objects/item.dart';
 import 'package:quickumber/templates/instock-list-item.dart';
 
-class InStock extends StatefulWidget
-{
+class InStock extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _InStockState();
 }
 
-class _InStockState extends State<InStock>
-{
-  final Item item = new Item("123", "https://picsum.photos/id/1080/200/300", "Rice", 55.0, "KG");
+class _InStockState extends State<InStock> {
+  final Item item = new Item(
+      "123", "https://picsum.photos/id/1080/200/300", "Rice", 55.0, "KG");
+  var items = [
+    new Item("100", "https://picsum.photos/id/1081/200/300", "Rice", 55.0, "KG"),
+    new Item("200", "https://picsum.photos/id/1082/200/300", "Rice", 100.0, "KG"),
+    new Item("300", "https://picsum.photos/id/1083/200/300", "Rice", 200.0, "GM"),
+    new Item("400", "https://picsum.photos/id/1084/200/300", "Rice", 40.0, "KG"),
+    new Item("500", "https://picsum.photos/id/139/200/300", "Rice", 200.0, "KG"),
+    new Item("600", "https://picsum.photos/id/1069/200/300", "Rice", 10.0, "KG"),
+    new Item("700", "https://picsum.photos/id/1070/200/300", "Rice", 2.0, "KG"),
+    new Item("800", "https://picsum.photos/id/1068/200/300", "Rice", 300.0, "KG"),
+    new Item("900", "https://picsum.photos/id/107/200/300", "Rice", 1000.0, "KG"),
+    new Item("1000", "https://picsum.photos/id/133/200/300", "Rice", 40.0, "KG"),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: Container(
-        child: ListView.separated(
-            //padding: const EdgeInsets.all(8),
-            itemCount: 10,
-            itemBuilder: (BuildContext context, int index) {
-              return ListItem(item);
-            },
-            separatorBuilder: (BuildContext context, int index) => const Divider(),
-        )
-      ),
+          child: ListView.separated(
+        //padding: const EdgeInsets.all(8),
+        itemCount: 10,
+        itemBuilder: (BuildContext context, int index) {
+          return ListItem(item: items[index], key: UniqueKey());
+        },
+        separatorBuilder: (BuildContext context, int index) => const Divider(
+          thickness: 3,
+        ),
+      )),
     );
   }
-
 }
--- lib/templates/instock-list-item.dart
+++ lib/templates/instock-list-item.dart
@@ -4,122 +4,190 @@ import 'package:quickumber/objects/item.dart';
 
 class ListItem extends StatefulWidget {
   Item item;
-
-  ListItem(this.item);
+  var key;
+  static var favoriteList = [];
+  ListItem({this.item, this.key}) : super(key: key);
 
   @override
-  State<StatefulWidget> createState() => _ListItemState(item);
+  State<StatefulWidget> createState() => _ListItemState(item, key);
 }
 
 class _ListItemState extends State<ListItem> {
+
+  var key;
   Item item;
+  Color likeButtonColor = Colors.black12;
+  int amount = 1;
 
-  _ListItemState(this.item);
+  addFavorite(String item) {
+    ListItem.favoriteList.add(item);
+      likeButtonColor = Colors.red;
+  }
 
-  Color likeButtonColor = Colors.grey;
+  removeFavorite(String item)
+  {
+    ListItem.favoriteList.remove(item);
+      likeButtonColor = Colors.black12;
+  }
+
+  _ListItemState(this.item, this.key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(10),
+        padding: EdgeInsets.all(10),
         height: 150,
         child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Expanded(
+            //IMAGE
+            Flexible(
               flex: 1,
-              child: Image.network(
-                this.item.displayImgUrl,
-                height: 100,
-                width: 100,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(
+                  this.item.displayImgUrl,
+                  fit: BoxFit.fill,
+                  height: 100,
+                  width: 100,
+                ),
               ),
             ),
-            Expanded(
-                flex: 2,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(this.item.displayName, style: TextStyle(fontWeight: FontWeight.bold),),
-                    Text(this.item.price.toString(), style: TextStyle(fontWeight: FontWeight.bold)),
-                    Container(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            height: 40,
-                            width: 40,
-                            child: Ink(
-                              decoration: ShapeDecoration(
-                                color: Colors.orange,
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10.0)),
-                              ),
-                              child: IconButton(
-                                onPressed: () {},
-                                icon: Icon(
-                                  Icons.remove,
-                                  color: Colors.white,
-                                ),
+
+            //CENTER-PORTION
+            Flexible(
+              flex: 2,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    this.item.displayName,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(this.item.price.toString(),
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                  Container(
+                    width: 150,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        /**** REMOVE_BUTTON ****/
+                        Container(
+                          height: 35,
+                          width: 35,
+                          child: Ink(
+                            decoration: ShapeDecoration(
+                              color: Colors.orange,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.0)),
+                            ),
+                            child: IconButton(
+                              iconSize: 20,
+                              onPressed: () {
+                                setState(() {
+                                  if(amount != 0)
+                                    amount--;
+                                });
+                              },
+                              icon: Icon(
+                                Icons.remove,
+                                color: Colors.white,
                               ),
                             ),
                           ),
-                          SizedBox(width: 20,),
-                          Text(this.item.unit, style: TextStyle(fontWeight: FontWeight.bold),),
-                          SizedBox(width: 20,),
-                          Container(
-                            height: 40,
-                            width: 40,
-                            child: Ink(
-                              decoration: ShapeDecoration(
-                                color: Colors.orange,
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10.0)),
-                              ),
-                              child: IconButton(
-                                // splashColor: Colors.transparent,
-                                // highlightColor: Colors.transparent,
-                                onPressed: () {},
-                                icon: Icon(
-                                  Icons.add,
-                                  color: Colors.white,
-                                ),
+                        ),
+                        // SizedBox(
+                        //   width: 20,
+                        // ),
+                        Text(
+                          amount.toString() + " " + this.item.unit,
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        // SizedBox(
+                        //   width: 20,
+                        // ),
+                        /**** ADD_BUTTON ****/
+                        Container(
+                          height: 35,
+                          width: 35,
+                          child: Ink(
+                            decoration: ShapeDecoration(
+                              color: Colors.orange,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.0)),
+                            ),
+                            child: IconButton(
+                              iconSize: 20,
+                              // splashColor: Colors.transparent,
+                              // highlightColor: Colors.transparent,
+                              onPressed: () {
+                                setState(() {
+                                  if(amount < 100)
+                                    amount++;
+                                });
+                              },
+                              icon: Icon(
+                                Icons.add,
+                                color: Colors.white,
                               ),
                             ),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                    SizedBox(height: 10,),
-                    Container(
-                      width: 170,
-                      height: 40,
-                      child: OutlinedButton(
-                        onPressed: () {},
-                        child: Text('Add to Cart', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),),
-                        style: OutlinedButton.styleFrom(
-                            primary: Colors.orange,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20.0))),
+                  ),
+                  // SizedBox(
+                  //   height: 15,
+                  // ),
+                  Container(
+                    width: 170,
+                    height: 40,
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Add to Cart',
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold),
                       ),
-                    )
-                  ],
-                )),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                OutlinedButton(onPressed: () {}, child: Text("\$10 Discount")),
-                IconButton(
-                    onPressed: () {
-                      setState(() {
-                        likeButtonColor == Colors.grey
-                            ? likeButtonColor = Colors.red
-                            : likeButtonColor = Colors.grey;
-                      });
-                    },
-                    icon: Icon(
-                      Icons.circle,
-                      color: likeButtonColor,
-                    ))
-              ],
+                      style: OutlinedButton.styleFrom(
+                          primary: Colors.orange,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0))),
+                    ),
+                  )
+                ],
+              ),
+            ),
+
+            //LAST PORTION
+            Flexible(
+              flex: 1,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                      height: 30,
+                      child: OutlinedButton(
+                          onPressed: () {},
+                          child: Text(
+                            "\$10 Discount",
+                            style: TextStyle(fontSize: 11.0),
+                          ))),
+                  IconButton(
+                      onPressed: () {
+                        setState(() {
+                          likeButtonColor == Colors.black12
+                              ? addFavorite(this.item.uid)
+                              : removeFavorite(this.item.uid);
+                        });
+                        print(ListItem.favoriteList);
+                      },
+                      icon: Icon(
+                        Icons.circle,
+                        color: likeButtonColor,
+                      ))
+                ],
+              ),
             )
           ],
         ));
